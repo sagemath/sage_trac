b3cd4386e45b160999dfc97943b49660dc2cab29
R. Andrew Ohana <ohanar@trac.(none)>
2013-03-27 18:06:28 -0700

    add sshkeys to gitolite

--- plugins/sshkeys.py
+++ plugins/sshkeys.py
@@ -5,6 +5,10 @@ from trac.prefs import IPreferencePanelProvider
 from trac.admin.api import IAdminCommandProvider
 from trac.util.text import printout
 from tracrpc.api import IXMLRPCHandler
+import subprocess, os
+
+_gitolite_keydir = '/home'+os.path.join('www-data', 'gitolite', 'keydir')
+_gitolite_update = '/home'+os.path.join('www-data','bin','gitolite-update')
 
 class UserDataStore(Component):
     def save_data(self, user, dictionary):
@@ -18,7 +22,7 @@ class UserDataStore(Component):
 	        try:
                     cursor.execute('INSERT INTO "user_data_store" VALUES (%s, %s, %s)', (user, key, value))
 		except:
-		    cursor.execute('REPLACE INTO "user_data_store" VALUES (%s, %s, %s)', (user, key, value)) 
+		    cursor.execute('REPLACE INTO "user_data_store" VALUES (%s, %s, %s)', (user, key, value))
 
     def get_data(self, user):
         """
@@ -56,23 +60,23 @@ class UserDataStore(Component):
 
 class SshKeysPlugin(Component):
     implements(IPreferencePanelProvider, IAdminCommandProvider, IXMLRPCHandler)
-    
+
     def __init__(self):
         self._user_data_store = UserDataStore(self.compmgr)
 
     # IPreferencePanelProvider methods
     def get_preference_panels(self, req):
         yield ('sshkeys', _('SSH keys'))
-    
+
     def render_preference_panel(self, req, panel):
         user = req.authname
         if req.method == 'POST':
-            new_ssh_key = req.args.get('ssh_key').strip()
+            new_ssh_keys = req.args.get('ssh_key').strip()
             if new_ssh_key:
-                self._setkey(user, new_ssh_key)
+                self._setkeys(user, new_ssh_keys)
                 add_notice(req, 'Your ssh key has been saved.')
             req.redirect(req.href.prefs(panel or None))
-    
+
         return 'prefs_ssh_keys.html', self._user_data_store.get_data(user)
 
     # IAdminCommandProvider methods
@@ -93,6 +97,22 @@ class SshKeysPlugin(Component):
     def _do_dump_key(self, user):
         printout(self._getkey(user))
 
+    # Gitolite exporting
+    def _export_to_gitolite(self, user, keys):
+        for i,key in enumerate(keys):
+            if i > 0xff:
+                #add_warning(req, 'We only support using your first 256 ssh keys.')
+                break
+            else:
+                d = hex(i)[2:]
+                while len(d) < 2:
+                    d = '0'+d
+                f = open(os.path.join(_gitolite_keydir, d, user+'.pub'), 'w')
+                f.write(key)
+                f.close()
+        process = subprocess.Popen(_gitolite_update)
+        process.wait()
+
     # general functionality
     def _listusers(self):
         all_data = self._user_data_store.get_data_all_users()
@@ -100,21 +120,23 @@ class SshKeysPlugin(Component):
             if data.has_key('ssh_key'):
                 yield user
 
-    def _getkey(self, user):
+    def _getkeys(self, user):
         return self._user_data_store.get_data(user)['ssh_key']
 
-    def _setkey(self, user, key):
-        self._user_data_store.save_data(user, {'ssh_key': key})
+    def _setkeys(self, user, keys):
+        self._user_data_store.save_data(user, {'ssh_key': keys})
+        self._export_to_gitolite(user, keys.split('\n'))
+
 
     # RPC boilerplate
     def listusers(self, req):
         return list(self._listusers())
 
-    def getkey(self, req):
-        return self._getkey(req.authname)
+    def getkeys(self, req):
+        return self._getkeys(req.authname)
 
-    def setkey(self, req, key):
-        return self._setkey(req.authname, key)
+    def setkeys(self, req, keys):
+        return self._setkeys(req.authname, keys)
 
     # IXMLRPCHandler methods
     def xmlrpc_namespace(self):
@@ -122,6 +144,6 @@ class SshKeysPlugin(Component):
 
     def xmlrpc_methods(self):
         yield (None, ((list,),), self.listusers)
-        yield (None, ((str,),), self.getkey)
-        yield (None, ((None,str),), self.setkey)
+        yield (None, ((str,),), self.getkeys)
+        yield (None, ((None,str),), self.setkeys)
 
